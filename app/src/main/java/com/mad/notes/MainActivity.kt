ede0bc809ab1388a51de494091e88644c2c63c3b
Mukesh Kumar Patel <mukesh.kumaar8293@gmail.com>
2020-10-27 12:56:35 +0530

    BaseActivity
    -Added Helpers.kt with null check methods
    - BaseActivity.kt with Internet broadcast receiver check

--- app/src/main/java/com/mad/notes/MainActivity.kt
+++ app/src/main/java/com/mad/notes/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.mad.notes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mad.notes.ui.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
