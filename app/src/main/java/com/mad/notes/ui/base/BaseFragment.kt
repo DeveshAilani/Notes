37e9688742c3e520f4004852575aa71a11d236dc
Devesh <devesh.ailani@gmail.in>
2020-10-29 19:46:31 +0530

    Create BaseFragment.kt

---
+++ app/src/main/java/com/mad/notes/ui/base/BaseFragment.kt
@@ -0,0 +1,89 @@
+package com.mad.notes.ui.base
+
+import android.app.Activity
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+/**
+ * @author Devesh Ailani <deveshailani9@gmail.com>
+ * @version $Revision 1.0 $, $Date 29/10/2020
+ * @since 1.0.0
+ */
+
+open class BaseFragment : Fragment() {
+
+    /**
+     * Instance of the base activity to which the fragment is attached;
+     */
+    protected var mActivity: BaseActivity? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        onAttachToContext(context)
+    }
+
+    /*
+    * Deprecated on API 23
+    * Use onAttachToContext instead
+    */
+    override fun onAttach(activity: Activity) {
+        super.onAttach(activity)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            onAttachToContext(activity)
+        }
+    }
+
+    /*
+     * Called when the fragment attaches to the context
+     */
+    protected open fun onAttachToContext(context: Context?) {
+        mActivity = context as BaseActivity?
+        mActivity?.let {
+            it.onFragmentAttached(this)
+        }
+    }
+
+    override fun onDetach() {
+        mActivity?.let {
+            it.onFragmentDetached(this)
+            mActivity = null
+        }
+        super.onDetach()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    /**
+     * Handles the back press event for the particular fragment
+     *
+     * @return whether the back press is consumed or not. Returns true if the action for back press is completed. Returns
+     * false when the fragment is not handling the event.
+     */
+    open fun onBackPressed(): Boolean {
+        val fragmentManager = childFragmentManager
+        if (fragmentManager.backStackEntryCount > 0) {
+            fragmentManager.popBackStackImmediate()
+            return true
+        }
+        return false
+    }
+
+    fun setUpActionBar(title: String?, isBack: Boolean) {
+        val actionBar = activity!!.actionBar
+        actionBar?.title = title
+        actionBar?.setDisplayHomeAsUpEnabled(isBack)
+    }
+}
